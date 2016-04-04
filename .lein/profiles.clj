6dfeff372f7e3290b2adcd0e867233ce7bb6ff02
Dan McKinley <mcfunley@gmail.com>
2016-04-04 11:59:31 -0700

    upgrading cider

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [[lein-kibit "0.1.2"]
                   [lein-ancient "0.6.8"]]}
- :repl {:plugins [[cider/cider-nrepl "0.10.1"]]
+ :repl {:plugins [[cider/cider-nrepl "0.11.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
